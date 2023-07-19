23a5858ec06dd9d4fd785831978b5a541350c45c
22022050 <22022050@myrp.edu.sg>
2023-07-19 23:18:46 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/tasklist/ToDoItem.java
@@ -0,0 +1,47 @@
+package com.myapplicationdev.android.tasklist;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
+
+public class ToDoItem {
+
+    private String title;
+    private Calendar date;
+
+    public ToDoItem(String title, Calendar date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public String toString() {
+        String str = date.get(Calendar.DAY_OF_MONTH)+"/"+date.get(Calendar.MONTH)
+                +"/"+date.get(Calendar.YEAR) + " (" + getDay(date.get(Calendar.DAY_OF_WEEK)) + ")";
+
+        return str;
+    }
+
+    private String getDay(int day) {
+
+        // Complete the code here
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        return daysOfWeek[day - 1];
+    }
+}
+
